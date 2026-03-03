31ecbd6a965c25caa3856c156a1371a8ca7b3f18
yemanealem <yemanea223@gmail.com>
2026-03-03 18:07:15 +0100

    fix:cross origin problem

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from fastapi import FastAPI
 from app.api.v1.quote import router as quote_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="OnsiteWash Backend")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://onsitewash-frontend-app-u7e5k.ondigitalocean.app",
+        "https://www.onsitespray.com.au",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(quote_router, prefix="/api/v1")
 
